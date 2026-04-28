7c7bfce8e28939fe66452ffbd2b7b4bd75bff002
Max Wofford <max@maxwofford.com>
2026-04-28 17:46:29 -0400

    Dry up shop controller

--- app/controllers/shop/items_controller.rb
+++ app/controllers/shop/items_controller.rb
@@ -45,26 +45,6 @@ class Shop::ItemsController < Shop::BaseController
 
   private
 
-  def load_shop_items
-    excluded_free_stickers = current_user && has_ordered_free_stickers?
-    shop_page_data = ShopItem.cached_shop_page_data
-    @shop_items = shop_page_data[:buyable_standalone]
-    @shop_items = @shop_items.reject { |item| item.type == "ShopItem::FreeStickers" } if excluded_free_stickers
-    @featured_item = featured_free_stickers_item unless excluded_free_stickers
-    @recently_added_items = shop_page_data[:recently_added]
-    @user_balance = current_user&.cached_balance || 0
-  end
-
-  def has_ordered_free_stickers?
-    current_user.has_gotten_free_stickers? ||
-      current_user.shop_orders.joins(:shop_item).where(shop_items: { type: "ShopItem::FreeStickers" }).exists?
-  end
-
-  def featured_free_stickers_item
-    item = ShopItem.find_by(id: 1, type: "ShopItem::FreeStickers", enabled: true)
-    item if item&.enabled_in_region?(@user_region)
-  end
-
   def grant_free_stickers_welcome_cookies!
     unless current_user.ledger_entries.exists?(reason: "Free Stickers Welcome Grant")
       current_user.ledger_entries.create!(
--- app/controllers/shop/regions_controller.rb
+++ app/controllers/shop/regions_controller.rb
@@ -20,25 +20,4 @@ class Shop::RegionsController < Shop::BaseController
     end
   end
 
-  private
-
-  def load_shop_items
-    excluded_free_stickers = current_user && has_ordered_free_stickers?
-    shop_page_data = ShopItem.cached_shop_page_data
-    @shop_items = shop_page_data[:buyable_standalone]
-    @shop_items = @shop_items.reject { |item| item.type == "ShopItem::FreeStickers" } if excluded_free_stickers
-    @featured_item = featured_free_stickers_item unless excluded_free_stickers
-    @recently_added_items = shop_page_data[:recently_added]
-    @user_balance = current_user&.cached_balance || 0
-  end
-
-  def has_ordered_free_stickers?
-    current_user.has_gotten_free_stickers? ||
-      current_user.shop_orders.joins(:shop_item).where(shop_items: { type: "ShopItem::FreeStickers" }).exists?
-  end
-
-  def featured_free_stickers_item
-    item = ShopItem.find_by(id: 1, type: "ShopItem::FreeStickers", enabled: true)
-    item if item&.enabled_in_region?(@user_region)
-  end
 end
