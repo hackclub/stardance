acedf8bf91ceda7f95912df82c69f7a73810a19b
Brian <72092838+BrianWalczak@users.noreply.github.com>
2026-04-28 22:53:18 -0500

    annoying db :[

--- db/migrate/20260429032609_rename_flavortown_metrics.rb
+++ db/migrate/20260429032609_rename_flavortown_metrics.rb
@@ -1,6 +1,15 @@
 class RenameFlavortownMetrics < ActiveRecord::Migration[8.1]
-  def change
-    rename_column :users, :flavortown_message_count_14d, :message_count_14d
-    rename_column :users, :flavortown_support_message_count_14d, :support_message_count_14d
+  def up
+    safety_assured do
+      rename_column :users, :flavortown_message_count_14d, :message_count_14d
+      rename_column :users, :flavortown_support_message_count_14d, :support_message_count_14d
+    end
+  end
+
+  def down
+    safety_assured do
+      rename_column :users, :message_count_14d, :flavortown_message_count_14d
+      rename_column :users, :support_message_count_14d, :flavortown_support_message_count_14d
+    end
   end
 end
