959db26e84e98a4a0344b71bf7d38c3183685d74
Kartikey Chauhan <me@cskartikey.dev>
2026-05-05 00:27:57 +0530

    feat(mailbox): add routing for stardance-inbound emails to rsvp reply

--- app/mailboxes/application_mailbox.rb
+++ app/mailboxes/application_mailbox.rb
@@ -2,5 +2,6 @@ class ApplicationMailbox < ActionMailbox::Base
   routing(/^tracking@/i => :tracking)
   routing(/^hcb@/i => :hcb)
   routing(/^rsvp@stardance\.hackclub\.com\z/i => :"rsvp/reply")
+  routing(/^stardance-inbound@stardance\.hackclub\.com\z/i => :"rsvp/reply")
   routing all: :incinerate
 end
