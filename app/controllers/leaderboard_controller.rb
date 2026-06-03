8472a44673f3452e0a2c68f9f9edd092b161e7a4
Abdallah Ebrahim (dracula) <abdallah.ebrahim.official@gmail.com>
2026-06-03 08:33:34 +0300

    feat: fetch top users for current and all-time

--- app/controllers/leaderboard_controller.rb
+++ app/controllers/leaderboard_controller.rb
@@ -4,7 +4,8 @@ class LeaderboardController < ApplicationController
                 .joins(:preference)
                 .where(user_preferences: { leaderboard_optin: true }, banned: false)
 
-    sorted_users = scope.sort_by { |u| -u.cached_balance }
-    @pagy, @users = pagy(:offset, sorted_users, limit: 10)
+    @current_users = scope.sort_by { |u| -u.cached_balance }.first(50)
+    @all_time_users = scope.sort_by { |u| -u.cached_total_earned }.first(50)
+    @total_count = scope.count
   end
 end
