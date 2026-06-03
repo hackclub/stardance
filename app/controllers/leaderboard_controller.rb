f67bd6e22c1f593fe2a204730c1b59a4b4b25496
Abdallah Ebrahim (dracula) <abdallah.ebrahim.official@gmail.com>
2026-06-03 09:10:36 +0300

    fix: make leaderboard shows only top 10

--- app/controllers/leaderboard_controller.rb
+++ app/controllers/leaderboard_controller.rb
@@ -4,8 +4,8 @@ class LeaderboardController < ApplicationController
                 .joins(:preference)
                 .where(user_preferences: { leaderboard_optin: true }, banned: false)
 
-    @current_users = scope.sort_by { |u| -u.cached_balance }.first(50)
-    @all_time_users = scope.sort_by { |u| -u.cached_total_earned }.first(50)
+    @current_users = scope.sort_by { |u| -u.cached_balance }.first(10)
+    @all_time_users = scope.sort_by { |u| -u.cached_total_earned }.first(10)
     @total_count = scope.count
   end
 end
