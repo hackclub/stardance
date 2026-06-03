bd2736a2520134fc796ec9f57ed379efec746289
Abdallah Ebrahim (dracula) <abdallah.ebrahim.official@gmail.com>
2026-06-03 08:32:30 +0300

    feat: add total_earned tracking and caching to User::Wallet

--- app/models/user/wallet.rb
+++ app/models/user/wallet.rb
@@ -3,11 +3,20 @@ module User::Wallet
 
   def balance = ledger_entries.sum(:amount)
 
+  def total_earned = ledger_entries.where("amount > 0").sum(:amount)
+
   def cached_balance = Rails.cache.fetch(balance_cache_key) { balance }
+
+  def cached_total_earned = Rails.cache.fetch(total_earned_cache_key) { total_earned }
 
   def balance_cache_key = "user/#{id}/sidebar_balance"
 
-  def invalidate_balance_cache! = Rails.cache.delete(balance_cache_key)
+  def total_earned_cache_key = "user/#{id}/total_earned_balance"
+
+  def invalidate_balance_cache!
+    Rails.cache.delete(balance_cache_key)
+    Rails.cache.delete(total_earned_cache_key)
+  end
 
   def grant_email
     hcb_email.presence || email
