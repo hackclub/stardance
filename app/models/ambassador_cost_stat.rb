33152b9e901ab52c0689243c24270e30e6b600bb
Parth <215315028+nystar1@users.noreply.github.com>
2026-06-05 02:09:13 +0530

    fix(api): annotations

--- app/models/ambassador_cost_stat.rb
+++ app/models/ambassador_cost_stat.rb
@@ -4,19 +4,19 @@
 #
 # Table name: ambassador_cost_stats
 #
-#  id                                    :bigint           not null, primary key
-#  admin_cost_cents                      :integer          default(0), not null
-#  ambassador_region_breakdown           :jsonb            not null
-#  approved_ambassadors_count            :integer          default(0), not null
-#  average_cost_per_ambassador_cents     :integer          default(0), not null
-#  office_grant_cost_cents               :integer          default(0), not null
-#  poster_cost_cents                     :integer          default(0), not null
-#  referral_cost_cents                   :integer          default(0), not null
-#  shirt_cost_cents                      :integer          default(0), not null
-#  synced_at                             :datetime         not null
-#  total_cost_cents                      :integer          default(0), not null
-#  created_at                            :datetime         not null
-#  updated_at                            :datetime         not null
+#  id                                :bigint           not null, primary key
+#  admin_cost_cents                  :integer          default(0), not null
+#  ambassador_region_breakdown       :jsonb            not null
+#  approved_ambassadors_count        :integer          default(0), not null
+#  average_cost_per_ambassador_cents :integer          default(0), not null
+#  office_grant_cost_cents           :integer          default(0), not null
+#  poster_cost_cents                 :integer          default(0), not null
+#  referral_cost_cents               :integer          default(0), not null
+#  shirt_cost_cents                  :integer          default(0), not null
+#  synced_at                         :datetime         not null
+#  total_cost_cents                  :integer          default(0), not null
+#  created_at                        :datetime         not null
+#  updated_at                        :datetime         not null
 #
 # Indexes
 #
