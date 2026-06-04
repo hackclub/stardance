2254a87a31ee7ca005960f58e292394737a81819
Dhamari Trice-Hanson <39872667+dhamariT@users.noreply.github.com>
2026-06-04 08:53:02 -0400

    feat(sidebar): use shipwright logo icon

--- app/components/sidebar_component.rb
+++ app/components/sidebar_component.rb
@@ -49,7 +49,7 @@ class SidebarComponent < ViewComponent::Base
 
     if signed_in? && user.can_review?
       items << { slug: "shipwrights", label: "shipwrights", path: helpers.admin_certification_ships_path,
-        icon: "ship", badge_count: ship_queue_badge_count }
+        icon: { idle: "shipwright", active: "shipwright" }, badge_count: ship_queue_badge_count }
     end
 
     if signed_in?
