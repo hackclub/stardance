5d230ba6b3eaa97a973be1a99b8cafbcdda0efa0
Abdallah Ebrahim (dracula) <abdallah.ebrahim.official@gmail.com>
2026-06-10 06:03:50 +0300

    fix(initializer): clearing signups & RSVPs cache on boot

--- config/initializers/warmup_caches.rb
+++ config/initializers/warmup_caches.rb
@@ -1,6 +1,11 @@
 Rails.application.config.after_initialize do
   next unless defined?(Rails::Server) || ENV["WARM_CACHE"] == "true"
 
+  Rails.logger.info "Clearing landing page RSVP and signup counts cache on application boot..."
+  Rails.cache.delete("landing/rsvp_count")
+  Rails.cache.delete("landing/signup_count")
+  Rails.logger.info "Landing page RSVP and signup counts cache cleared."
+
   Rails.logger.info "Warming up sitemap cache..."
   Cache::SitemapJob.perform_later
 end
